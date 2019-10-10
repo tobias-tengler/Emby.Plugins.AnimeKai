8ef36f33778f560e30817576298c748e662673f6
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2019-10-10 15:38:04 +0200

    Initial Commit

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+using System;
+using MediaBrowser.Common.Plugins;
+
+namespace Emby.Providers.Anime
+{
+    public class Plugin : BasePlugin
+    {
+        public override Guid Id => new Guid("ce69a5ea-14b6-44a3-b75a-9d22dd92a7cf");
+
+        public override string Name => "AnimeProvider";
+
+        public override string Description => "";
+    }
+}
